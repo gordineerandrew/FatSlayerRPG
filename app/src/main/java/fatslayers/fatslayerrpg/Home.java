3846d665726d5310dfaa19fec845c3bab1853ab6
gordineerandrew <gordineerandrew@gmail.com>
2017-07-30 17:16:13 -0500

    Fresh Start.

---
+++ app/src/main/java/fatslayers/fatslayerrpg/Home.java
@@ -0,0 +1,22 @@
+package fatslayers.fatslayerrpg;
+
+/**
+ * Created by drako on 7/30/2017.
+ */
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Home extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.home_screen, container, false);
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/fatslayers/fatslayerrpg/Quest.java
@@ -0,0 +1,22 @@
+package fatslayers.fatslayerrpg;
+
+/**
+ * Created by drako on 7/30/2017.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Quest extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.quest_screen, container, false);
+        return rootView;
+    }
+
+}
